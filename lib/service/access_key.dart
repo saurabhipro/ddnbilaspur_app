c652c52f6a49041e1639d877a4d5b43594dc5f60
Amitanshu Singh <singh.amitanshu@gmail.com>
2023-09-05 09:48:52 +0530

    google map: restored authentication method.

--- lib/service/access_key.dart
+++ lib/service/access_key.dart
@@ -9,7 +9,6 @@ class AccessKeyStorage {
   }
 
   static Future<String?> getAccessToken() async {
-    return 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbWl0YW5zaHUiLCJhdXRoIjoiUk9MRV9BUkkiLCJleHAiOjE2OTM5NjU4Mzh9.6PIPmikg9tuDja47wQkO59rFOTmxAU6odwxE8Dxs1uhxocuhSM3b0N20z3UFh8DCFeqH9acFMt73KCVgfR_oEg';
-    //return await storage.read(key: AppConstant.jwtKeyName);
+    return await storage.read(key: AppConstant.jwtKeyName);
   }
 }
